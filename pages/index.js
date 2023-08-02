a7cdcaeac46c7d94fb5a8e9f98b6804570fb0bab
Hirok Jyoti Nath <hirok3044@gmail.com>
2023-08-02 09:52:40 +0530

    api left

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import MeetupList from '@/components/meetups/MeetupList';
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+import { MongoClient} from 'mongodb';
+
+import MeetupList from '@/components/meetups/MeetupList';
+
+const HomePage = (props) => {
+  return (
+    <Fragment>
+        <Head>
+            <title>MeetUps</title>
+            <meta name='description'
+                  content='Browse a huge list of highly active React meetups'
+            />
+        </Head>
+        <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+    const client = await MongoClient.connect(URL);
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
+    return {
+        props: {
+            meetups: meetups.map((meetup) => ({
+                title:meetup.title,
+                address:meetup.address,
+                image:meetup.image,
+                id:meetup._id.toString(),
+            })),
+        },
+        revalidate: 1,
+    };
+}
+
+export default HomePage;
